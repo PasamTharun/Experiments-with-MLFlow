179bc5e37d94b9a38c70e39c41a47b18222fc6fd
PasamTharun <tpasamtharun@gmail.com>
2026-01-05 00:56:56 +0530

    updated till mlflow autolog

---
+++ mlartifacts/1/716c4ad248874818ba897543c2a8d569/artifacts/file1.py
@@ -0,0 +1,56 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+mlflow.set_tracking_uri('http://localhost:5000') 
+
+#load wine dataset
+wine=load_wine()
+X=wine.data
+y=wine.target
+
+#train test split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.10,random_state=42)
+
+#define the params for RF model
+max_depth=5
+n_estimators=20
+
+
+#mention your experiment below
+#mlflow.set_experiment('Mlops-Exp1') #we can also add the experiment by using id 
+
+
+with mlflow.start_run(experiment_id='1'): 
+    rf= RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+    rf.fit(X_train,y_train)
+    
+    y_pred = rf.predict(X_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+    
+    # creating the connfusion matrix '
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True,fmt='d',cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    
+    # save the plot
+    plt.savefig('confusion_matrix.png')
+    
+    #LOG artifacts using mlflow
+    mlflow.log_artifact('confusion_matrix.png')
+    mlflow.log_artifact(__file__)   # we can also do this by git add and git commit
+    
+    print(accuracy)
